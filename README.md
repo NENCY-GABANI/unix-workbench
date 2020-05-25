438db02a5a34dc752e9bee5cb4253d80d1ce9fb1
NENCY-GABANI <gabani.rasikbhai@mca.christuniversity.in>
2020-05-25 14:15:57 +0530

    modified

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Peer-graded Assignment: Bash, Make, Git, and GitHub
-Mon May 25 00:42:55 IST 2020
-18
- 
-
-
+Peer-graded Assignment: Bash, Make, Git, and GitHub
+Mon May 25 14:15:10 DST 2020
+27
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,19 +1,27 @@
-g=$(ls | wc -l)
-c=0
-echo " ENTER NUMBER FOR GUESS : "
-read guessno
-while(($guessno -ne "q")) do
-	if[$guessno -lt $g] then 
-		echo " !!!!! GUESS IS TOO LOW !!!!!  "
-		c=c+1
-	elif[$guessno -gt $g] then
-		echo " !!!!! GUESS IS TOO HIGH !!!!!! "
-		c=c+1
+function guess {
+
+local count=$(ls . | wc -w) 
+local condition=1
+local answ=0
+
+while [[ $condition -eq 1 ]]
+do
+	echo "guess the number of files in this directory?"
+	read answ
+	if [[ $answ -eq $count ]]
+		
+		then condition=0
+		echo "Congratulations! Correct answer!"
+	elif [[ $answ -lt $count ]]
+		then
+		echo "too low"
 	else
-		echo " ----- CONGRATULATIONS PERFECT GUESS NO IS $guessno ----- "
-		break
+		echo "too high"
 	fi
 done
-echo " YOUR TOTAL NO OF TRIES : $c "
-exit
+
+
+}
+
+guess
 
